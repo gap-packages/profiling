fd2da66f575bdad99ba62f722512a45bc3ee8135
Max Horn <max@quendi.de>
2025-06-20 00:09:10 +0200

    Replace compiled.h by gap_all.h
    No functional change for this package but makes it future-proof.

--- src/gap_cpp_headers/gap_wrapping.hpp
+++ src/gap_cpp_headers/gap_wrapping.hpp
@@ -3,7 +3,7 @@
 #ifndef _GAP_WRAP_HPP_AQD
 #define _GAP_WRAP_HPP_AQD
 
-#include "compiled.h"   // GAP headers
+#include "gap_all.h"   // GAP headers
 
 #include "gap_prototypes.hpp"
 #include "gap_exception.hpp"
